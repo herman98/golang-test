f509a83ef08779520f4d2d55911ca6239d0a65a8
Herman98 <bhrs_te98@yahoo.com>
2017-06-08 16:50:44 +0700

    add api service for personal data

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # golang-test
 go Language Test
+
+
+
+Please Do the following requirements before running the service
+----
+
+Setting Docker for mysql
+----------
+$ docker run -p 3306:3306 --name test-mysql  -e MYSQL_USER=root -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DB=testDB -d mysql:latest
+$ docker ps -a
+$ docker start test-mysql
+
+install and setting mysql without docker
+------
+$ sudo apt-get install mysql-server
+
+
+install go 
+-----------
+$ sudo add-apt-repository ppa:gophers/go
+$ sudo apt-get update
+$ sudo apt-get install golang-stable
+
+set go-path
+--------
+$ export GOPATH=/home/herman/App/golang/git
+$ export PATH=$PATH::/usr/local/go/bin
+$ export PATH=$PATH:$GOPATH/bin
+
+
+go get package
+-----------------
+$ go get "github.com/go-sql-driver/mysql"
+$ go get "github.com/gin-gonic/gin"
+
+running service
+-----
+$ go run microservice.go
+
+or using build
+-----
+$ go build microservice.go
+$ ./microservice
+
---
+++ src/api/microservice.go
@@ -0,0 +1,202 @@
+/*
+	HermanSyah Property using GNU License
+*/
+package main
+
+import (
+	"math/rand"
+	"bytes"
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandStringBytes(n int) string {
+    b := make([]byte, n)
+    for i := range b {
+        b[i] = letterBytes[rand.Intn(len(letterBytes))]
+    }
+    return string(b)
+}
+
+
+func createDB(){
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/testDB")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer db.Close()
+	// make sure connection is available
+	err = db.Ping()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	stmt, err := db.Prepare("CREATE TABLE person (id int NOT NULL AUTO_INCREMENT, slug varchar(6), first_name varchar(40), last_name varchar(40), PRIMARY KEY (id));")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	_, err = stmt.Exec()
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Person Table successfully migrated....")
+	}
+}
+
+func main() {
+    str_rand := RandStringBytes(6)
+    fmt.Printf("%s\n" , str_rand)
+
+    //migrate models into db table
+    createDB();
+
+    db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/testDB")
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer db.Close()
+	// make sure connection is available
+	err = db.Ping()
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	type Person struct {
+		Id         int
+		Slug   	   string
+		First_Name string
+		Last_Name  string
+	}
+
+    router := gin.Default()
+	// Add API handlers here
+    // GET a person detail
+	router.GET("/person/:slug", func(c *gin.Context) {
+		var (
+			person Person
+			result gin.H
+		)
+		slug := c.Param("slug")
+		row := db.QueryRow("select id, slug, first_name, last_name from person where slug = ?;", slug)
+		err = row.Scan(&person.Id, &person.Slug, &person.First_Name, &person.Last_Name)
+		if err != nil {
+			// If no results send null
+			result = gin.H{
+				"result": nil,
+				"count":  0,
+			}
+		} else {
+			result = gin.H{
+				"result": person,
+				"count":  1,
+			}
+		}
+		c.JSON(http.StatusOK, result)
+	})
+
+	// GET all persons
+	router.GET("/persons", func(c *gin.Context) {
+		var (
+			person  Person
+			persons []Person
+		)
+		rows, err := db.Query("select id, slug, first_name, last_name from person;")
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		for rows.Next() {
+			err = rows.Scan(&person.Id, &person.Slug, &person.First_Name, &person.Last_Name)
+			persons = append(persons, person)
+			if err != nil {
+				fmt.Print(err.Error())
+			}
+		}
+		defer rows.Close()
+		c.JSON(http.StatusOK, gin.H{
+			"result": persons,
+			"count":  len(persons),
+		})
+	})
+
+	// POST new person details
+	router.POST("/person", func(c *gin.Context) {
+		var buffer bytes.Buffer
+		slug := RandStringBytes(6)
+		first_name := c.PostForm("first_name")
+		last_name := c.PostForm("last_name")
+		stmt, err := db.Prepare("insert into person (slug, first_name, last_name) values(?,?,?);")
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		_, err = stmt.Exec(slug, first_name, last_name)
+
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+
+		// Fastest way to append strings
+		buffer.WriteString(slug)
+		buffer.WriteString(" ")
+		buffer.WriteString(first_name)
+		buffer.WriteString(" ")
+		buffer.WriteString(last_name)
+		defer stmt.Close()
+		name := buffer.String()
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf(" %s successfully created", name),
+		})
+	})
+
+	// PUT - update a person details
+	router.PUT("/person", func(c *gin.Context) {
+		var buffer bytes.Buffer
+		id := c.Query("id")
+		first_name := c.PostForm("first_name")
+		last_name := c.PostForm("last_name")
+		stmt, err := db.Prepare("update person set first_name= ?, last_name= ? where id= ?;")
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		_, err = stmt.Exec(first_name, last_name, id)
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+
+		// Fastest way to append strings
+		buffer.WriteString(first_name)
+		buffer.WriteString(" ")
+		buffer.WriteString(last_name)
+		defer stmt.Close()
+		name := buffer.String()
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("Successfully updated to %s", name),
+		})
+	})
+
+	// Delete resources
+	router.DELETE("/person", func(c *gin.Context) {
+		id := c.Query("id")
+		stmt, err := db.Prepare("delete from person where id= ?;")
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		_, err = stmt.Exec(id)
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("Successfully deleted user: %s", id),
+		})
+	})
+
+	//run server
+	router.Run(":8080")
+}
+
+
